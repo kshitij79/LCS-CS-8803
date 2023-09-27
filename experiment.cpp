cbc5db12bb87cfe562b811b53b529717cb8a2a33
Pathania, Kshitij <kpathania3@gatech.edu>
2023-09-27 02:01:05 -0400

    Update experiment.cpp

--- experiment.cpp
+++ experiment.cpp
@@ -5,8 +5,25 @@
 #include <chrono>
 #include <random>
 # include "dpll.h"
+#include <future>
+#include <chrono>
+#include <thread>
+#include <fstream> // Added for file operations
 
-// The DPLL algorithm and simplify function go here...
+template<typename Func, typename... Args>
+auto runWithTimeout(Func&& func, int timeoutInSeconds, Args&&... args) -> std::invoke_result_t<Func, Args...>
+{
+    // Run the function in a separate thread and get the future
+    auto future = std::async(std::launch::async, std::forward<Func>(func), std::forward<Args>(args)...);
+
+    // Wait for the result or a timeout
+    if(future.wait_for(std::chrono::seconds(timeoutInSeconds)) == std::future_status::timeout) {
+        throw std::runtime_error("Timeout reached!");
+    }
+
+    // Retrieve the result
+    return future.get();
+}
 
 std::vector<std::vector<int>> generateRandom3SAT(int N, int L) {
     std::random_device rd;
@@ -30,43 +47,99 @@ std::vector<std::vector<int>> generateRandom3SAT(int N, int L) {
     return formula;
 }
 
-int numSplittingApplications = 0; // A global counter for the splitting applications
-
-bool DPLLWrapper(std::vector<std::vector<int>>& clauses) {
+bool DPLLWrapper(std::vector<std::vector<int>>& clauses, int & numSplittingApplications, std::chrono::duration<double> & time, std::string procedure) {
     std::unordered_map<int, bool> assignment;
     auto start = std::chrono::high_resolution_clock::now();
     
-    bool result = randomHeuristicDPLL(clauses, assignment);
-    
+    bool result;
+    if(procedure == "maxLiteral")
+        result = twoClauseHeuristicDPLL(clauses, assignment, numSplittingApplications);
+    else if(procedure == "random")
+        result = randomHeuristicDPLL(clauses, assignment, numSplittingApplications);
+    else if(procedure == "simple")
+        result = simpleHeuristicDPLL(clauses, assignment, numSplittingApplications);    
+    else
+        result = twoClauseHeuristicDPLL(clauses, assignment, numSplittingApplications);   
+
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = end-start;
-    std::cout << "Time taken: " << diff.count() << " s" << std::endl;
-    std::cout << "Number of splitting-rule applications: " << numSplittingApplications << std::endl;
+    time = diff;
     
+    // std::this_thread::sleep_for(std::chrono::seconds(5));
+
     return result;
 }
 
 int main() {
-    // std::vector<double> ratios = {3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8, 6.0};
-    std::vector<double> ratios = {3.2};
 
-    int N = 65; // Adjust as needed
-    int runs = 1;
-    
+    std::ofstream outputFile("results.csv"); // Create a CSV file for output
+
+    if (!outputFile.is_open()) {
+        std::cerr << "Failed to open the output file." << std::endl;
+        return 1;
+    }
+
+    outputFile << "Procedure,Ratio,MedianTime,MedianSplits,NoTimeout,SatisfiabilityRatio\n";
+
+    // std::vector<double> ratios = {3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8, 6.0};
+    std::vector<double> ratios = {3.2, 3.4, 3.6};
+
+    int N = 100; // Adjust as needed
+    int runs = 100;
+    std::vector<std::string> procedures = {"twoClause", "random", "maxLiteral"};
+
     for (double ratio : ratios) {
         int L = static_cast<int>(ratio * N);
-        int satisfiableCount = 0;
-        
-        for (int i = 0; i < runs; ++i) {
-            std::vector<std::vector<int>> formula = generateRandom3SAT(N, L);
-            bool isSatisfiable = DPLLWrapper(formula);
-            if (isSatisfiable) {
-                satisfiableCount++;
+        std::vector<std::vector<std::vector<int>>> formula;
+        for (std::string procedure: procedures) {
+            int satisfiableCount = 0;
+            int noTimeoutCount = 0;
+            std::vector<int> splitsData;           // For storing splits for each run.
+            std::vector<double> timeData;          // For storing time for each run.
+            
+            for(int i = 0; i < runs; ++i){
+                if(formula.size()<=i)
+                    formula.push_back(generateRandom3SAT(N, L));
+                
+                int noSplits = 0;
+                std::chrono::duration<double> time(0.0);
+                try {
+                    bool isSatisfiable = runWithTimeout([&]() {
+                                            return DPLLWrapper(formula[i], noSplits, time, procedure);
+                                        }, 1);
+                    if (isSatisfiable) {
+                        satisfiableCount++;
+                    }
+                    splitsData.push_back(noSplits);
+                    timeData.push_back(time.count());
+                } catch (const std::exception& e) {
+                    splitsData.push_back(10000);
+                    timeData.push_back(10000);
+                    noTimeoutCount++; 
+                }
             }
+            
+            std::sort(splitsData.begin(), splitsData.end());
+            std::sort(timeData.begin(), timeData.end());
+            // Compute medians
+            double medianSplits = (runs % 2 == 0) ? (splitsData[runs/2 - 1] + splitsData[runs/2]) / 2.0
+                                                : splitsData[runs/2];
+
+            double medianTime = (runs % 2 == 0) ? (timeData[runs/2 - 1] + timeData[runs/2]) / 2.0
+                                            : timeData[runs/2];
+
+            outputFile << procedure << "," << ratio << "," << medianTime << "," << medianSplits << "," << noTimeoutCount << ","
+                       << static_cast<double>(satisfiableCount) / static_cast<double>(runs) << "\n";
+            // std::cout << "Procedure Name:" << procedure << std::endl;
+            // std::cout << "Time taken: " << medianTime << " s" << std::endl;
+            // std::cout << "Number of splitting-rule applications: " << medianSplits << std::endl;
+            // std::cout << "For L/N ratio " << ratio << ", Satisfiable Probability: "<< satisfiableCount<<" " << static_cast<double>(satisfiableCount) / static_cast<double>(runs) << std::endl;
+            // std::cout << "Procedure Ends" << std::endl;
         }
         
-        std::cout << "For L/N ratio " << ratio << ", Satisfiable Probability: " << static_cast<double>(satisfiableCount) / runs << std::endl;
     }
     
+    outputFile.close();
+
     return 0;
 }
